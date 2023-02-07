b1e1aa71d66c2b45ab559d9bfd48bdc72c79bd51
Raquell2005 <raqdezotti@gmail.com>
2023-02-07 20:25:44 -0300

    Brabo B)

--- app/src/main/java/br/com/etecia/digimonmaster/MainActivity.java
+++ app/src/main/java/br/com/etecia/digimonmaster/MainActivity.java
@@ -3,12 +3,17 @@ package br.com.etecia.digimonmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView idListaValores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idListaValores = findViewById(R.id.idListaValores);
+
     }
 }
