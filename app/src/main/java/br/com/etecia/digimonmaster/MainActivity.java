6688ec3a4f56e8e9530136e50223cf3f57d80329
Raquell2005 <raqdezotti@gmail.com>
2023-02-14 21:34:21 -0300

    Completando A listview

--- app/src/main/java/br/com/etecia/digimonmaster/MainActivity.java
+++ app/src/main/java/br/com/etecia/digimonmaster/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.digimonmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView idListaValores;
+
+    String titulo[] = {"Agumon", "Patamon", "Piyomon", "Koromon"};
+    String descricao[] = {"Agumon is a Reptile Digimon. It has grown up and become able to walk on two legs, and has an appearance like a tiny dinosaur",
+    "Um Digimon Mamífero caracterizado pelas suas grandes orelhas. Consegue utilizá-as como asas e voar, mas, como só vai a uma velocidade de 1 km/h, diz-se que mais lhe vale andar.",
+    "um digimon pintainho cujas asas se desenvolveram de modo a parecerem braços.", "Um pequeno Digimon que derramou a penugem que cobria a sua superfície e cujo corpo ficou maior. Ainda é incapaz de lutar."};
+    int imagem[] = {R.drawable.agumon, R.drawable.patamon, R.drawable.piyomon, R.drawable.koromon};
+    double rating[] = {0.4, 1.0, 2.0, 0.7};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return imagem.length;
         }
 
         @Override
@@ -41,15 +50,31 @@ public class MainActivity extends AppCompatActivity {
         public long getItemId(int i) {
             return 0;
         }
-
+        @SuppressLint("MissingInflatedId")
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            TextView MTitulo, MDescricao;
-            RatingBar MRating;
-            ImageView MImagem;
+            //Variaveis do modelo
+            TextView DigimonTitulo, DigimonDescricao;
+            RatingBar DigimonRating;
+            ImageView DigimonImagem;
+
+            //Ligando o modelo com o adaptador
+            View v = getLayoutInflater().inflate(R.layout.modelo_digimon, null);
 
 
-            return null;
+            //Apresentado as variavéis
+            DigimonImagem = v.findViewById(R.id.MImagem);
+            DigimonTitulo = v.findViewById(R.id.MTitulo);
+            DigimonDescricao = v.findViewById(R.id.MDescricao);
+            DigimonRating = v.findViewById(R.id.MRating);
+
+            //Inserindo os valores das variáveis do Java
+            DigimonTitulo.setText(titulo[i]);
+            DigimonImagem.setImageResource(imagem[i]);
+            DigimonDescricao.setText(descricao[i]);
+            DigimonRating.setRating((float)rating[i]);
+
+            return v;
         }
     }
 }
